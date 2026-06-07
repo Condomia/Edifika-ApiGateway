c16d9a35f87e93e8baf654d3b9639823049da23b
Antuanete01 <u202310358@upc.edu.pe>
2026-06-07 10:47:23 -0500

    feat: first commit

---
+++ src/main/java/com/edifika/apigateway/EdifikaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.edifika.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdifikaApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdifikaApiGatewayApplication.class, args);
+    }
+
+}
